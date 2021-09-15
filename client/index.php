73663a50cb8b959f3da72a4e638bd5e3fca8a4e3
SIGUI Kessé Emmanuel (https://sikessem.com) <developer@sikessem.com>
2021-09-15 03:24:15 +0200

    :tada: Begin the project

---
+++ client/index.php
@@ -0,0 +1,19 @@
+<?php
+$app = 'SIKessEm Packager';
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <base href="/"/>
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon"/>
+    <link rel="icon" href="favicon.ico" type="image/x-icon"/>
+    <title><?= $app ?></title>
+  </head>
+  <body>
+    <h1><?= $app ?></h1>
+    <p>Welcome to <?= $app ?></p>
+  </body>
+</html>
